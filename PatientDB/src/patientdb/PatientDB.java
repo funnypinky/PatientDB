713bd55f6842849f0b9b77c8824386825cc39586
user <user@DESKTOP-799LK9P.fritz.box>
2018-06-11 20:43:10 +0200

    Beginning adding Modern Theme

--- PatientDB/src/patientdb/PatientDB.java
+++ PatientDB/src/patientdb/PatientDB.java
@@ -19,7 +19,7 @@ public class PatientDB extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("MainView.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("view/MainView.fxml"));
         
         Scene scene = new Scene(root);
         
